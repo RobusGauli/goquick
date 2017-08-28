070fc661f362033e813e1af7ccc68791cd32e2de
Robus <robusgauli@gmail.com>
2017-08-28 19:54:57 +0545

    Testing

--- test.go
+++ test.go
@@ -1,8 +1,11 @@
 package main
 import "fmt"
-func main() {
-a := 45
-b := 56
+func main() {a := 45
+b := 456
 fmt.Println(a + b)
 ;;
+alist := []int {4,5,6,7,8,}
+fmt.Println(alist)
+;;
+alist := []int {4,5,6,7}
 }
