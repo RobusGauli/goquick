e906a886217ea1fc3f93ac418fa6e62e1c872878
Robus <robusgauli@gmail.com>
2017-08-30 14:12:50 +0545

    test

--- test.go
+++ test.go
@@ -1,39 +1,5 @@
 package main
-imporimport "impor"
+import "fmt"
 func main() {
-    
-}
-adasdimport "imporimport"
-import ""impor""
-import "func"
-import "main()"
-import "{"
-import "}"
-import "adasd"
-func main() {
-    import "imporimport"
-import ""impor""
-import "func"
-import "main()"
-import "{"
-import "}"
-import "adasdimport"
-import ""imporimport""
-import "import"
-import """impor"""
-import "import"
-import ""func""
-import "import"
-import ""main()""
-import "import"
-import ""{""
-import "import"
-import ""}""
-import "import"
-import ""adasd""
-import "func"
-import "main()"
-import "{"
-import
-
+    fmt.Println("this is coole")
 }
